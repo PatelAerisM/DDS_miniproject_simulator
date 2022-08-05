198650344e9b0454405e74a9dbc2d4cb3ae794cc
PatelAerisM <93403587+PatelAerisM@users.noreply.github.com>
2022-08-05 22:43:39 +0530

    first commit by aeris

---
+++ dds-mini-project-report-master/assets/scripts/question.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "Stuck-at-0 or Stuck-at-1 is a type of registance in circuit ?",
+        answer: "True",
+        options: [
+            "True",
+            "False"
+        ]
+    },
+    {
+        numb: 2,
+        question: "&#34;Must have one or the other but not both&#34;, this statement is correct for which gate ?",
+        answer: "XOR",
+        options: [
+            "OR",
+            "XOR",
+            "AND",
+            "XNOR"
+        ]
+    },
+    {
+        numb: 3,
+        question: "With three or more variables XOR gate is equal to 1.",
+        answer: "If an even number of variables must be equal to 1.",
+        options: [
+            "If an even number of variables must be equal to 1.",
+            "If an odd number of variables must be equal to 1.",
+            "Both the above options.",
+            "We can not determine with the given data."
+        ]
+    },
+    {
+        numb: 4,
+        question: "The expression of an XOR gate is ",
+        answer: "A'B + AB'",
+        options: [
+            "A'B + AB'",
+            "3 bits",
+            "A + A.B",
+            "A' + B'"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In a wire, how many possibilities are there ?",
+        answer: "All of these",
+        options: [
+            "Stuck-at-0-fault",
+            "Stuck-at-1-fault",
+            "No fault",
+            "All of these"
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
